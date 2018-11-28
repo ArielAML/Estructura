28b146404b52ed8e27ab2623f9bbb42dede10b0a
ArielAML <43397738+ArielAML@users.noreply.github.com>
2018-11-27 22:02:49 -0800

    Add files via upload

---
+++ P5_2_MonroyLopezArielAlejandro/P5_2_MonroyLopezArielAlejandro/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P5_2_MonroyLopezArielAlejandro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double [] uno = { 11, 2, 3, 14, 18, 45, -3, -1, 6, 9 }; //vector con datos predeterminados
+            QuickSort uno1 = new QuickSort(uno, 9); //Constructor donde se envian los parametros para la clase QuickSort
+            uno1.quickso(uno, 0, 9); //Se mandan los parametros necesarios para que el metodo pueda ser recursivo
+            uno1.mostrar(); //Metodo que muestra los datos del vector resultante
+
+            double[] dos = { 1, 3, 5, 7, 8, 3, 9, .7071, 16.5664, 12, 0, 10 };
+            QuickSort dos2 = new QuickSort(dos, dos.Count());
+            dos2.quickso(dos, 0, 11);
+            dos2.mostrar();
+
+            double[] tres = { 3, 7, 15, 1.3333333333, 155, 100, 15, Math.Sqrt(3), 5, 2, 3, 2 };
+            QuickSort tres3 = new QuickSort(tres, tres.Count());
+            tres3.quickso(tres, 0, 11);
+            tres3.mostrar();
+
+            double[] cuatro = { 8, 19, 7, 3, 15, 23, 34, 27, 89, 101 };
+            QuickSort cuatro4 = new QuickSort(cuatro, cuatro.Count());
+            cuatro4.quickso(cuatro, 0, 9);
+            cuatro4.mostrar();
+        }
+    }
+}
---
+++ P5_2_MonroyLopezArielAlejandro/P5_2_MonroyLopezArielAlejandro/QuickSort.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P5_2_MonroyLopezArielAlejandro
+{
+    public class QuickSort
+    {
+        int longitud; //Propiedad para determinar la longitud del vector (este dato es solo necesario en el metodo mostrar)
+        double[] vector;//Donde se almacenan los numeros a ordenar
+
+        public QuickSort(double[] arreglo, int longi) //Metodo constructor
+        {
+            vector = arreglo;
+            longitud = longi;
+        }
+
+        public void quickso(double[] arreglo, int primer, int ultimo)
+            //Metodo recursivo que recive el vector original y modifica el orden de los numeros segun las condiciones
+        {
+            int menor = primer;
+            int mayor = ultimo;
+            double pivote = arreglo[(primer + ultimo) / 2];
+            double auxiliar;
+
+            do //mientras que el valor menor sea menos que el valor mayor, se acomodaran los datos del arreglo
+            {
+                while (arreglo[menor] < pivote) menor++;
+                while (arreglo[mayor] > pivote) mayor--;
+                if (menor <= mayor)
+                {
+                    auxiliar = arreglo[mayor];
+                    arreglo[mayor] = arreglo[menor];
+                    arreglo[menor] = auxiliar;
+                    menor++;
+                    mayor--;
+                }
+            } while (menor <= mayor);
+            if (primer < mayor) quickso(arreglo, primer, mayor); //si el numero menor original es mas bajo a mayor(valor modificado en el metodo), se hace recursividad para reordenamiento
+            if (ultimo > menor) quickso(arreglo, menor, ultimo); //Si el numero mayor original es mas alto a menor(valor modificado en el metodo), se hace recursividad para reordenamiento
+        }
+
+        public void mostrar()
+        {
+            Console.Clear();
+            for (int i = 0; i < longitud; i++)
+            {
+                Console.Write(" " + vector[i] + " => "); //Muestra los valores ya ordenados dentro del vector
+            }
+            Console.ReadLine();
+        }
+    }
+}
